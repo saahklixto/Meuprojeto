12fcb31dfa337631280b1a6703e0e7c89ece3556
Samara Calixto <samara.klixto@hotmail.com>
2023-05-30 13:35:15 -0300

    Adicionar arquivos de projeto.

---
+++ Meuprojeto/Program.cs
@@ -0,0 +1,100 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+List<string> listasDasBandas = new List<string>();
+
+void ExibirLogo ()
+{
+    Console.WriteLine(@"
+
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+    
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a media de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\n Digite sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: RegistrarBanda();
+            break;
+        case 2: MostrarBandasRegistradas ();
+            
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case -1:
+            Console.WriteLine("Tchau Tchau :) ");
+            break;
+        default:
+            Console.WriteLine("Opção invalida");
+            break;
+
+    }
+    
+    }
+
+void RegistrarBanda()
+
+{
+    Console.Clear();
+    Console.WriteLine("******************");
+    Console.WriteLine("Registro de bandas");
+    Console.WriteLine("******************\n");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    listasDasBandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+
+}
+
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    Console.WriteLine("******************");
+    Console.WriteLine("Bandas Registradas");
+    Console.WriteLine("******************\n");
+    //for (int i = 0; i < listasDasBandas.Count; i++)
+    //{
+      //  Console.WriteLine($"Banda: {listasDasBandas[i]}");
+    //}
+    foreach(string banda in listasDasBandas)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+
+    Console.WriteLine("Digite uma tecla para voltar ao menu");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+
+}
+
+
+ExibirOpcoesDoMenu ();
+ 
